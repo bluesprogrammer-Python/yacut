7983dd62ac18dbf8944bfe323f0a61f6599e92fa
Semenov Sergey <Raze.teatro2012@yandex.ru>
2023-10-10 01:35:22 +0300

    delete space

--- settings.py
+++ settings.py
@@ -6,4 +6,3 @@ class Config(object):
                                         default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv(key='SECRET_KEY', default='MY_SECRET_KEY')
-
